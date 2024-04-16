020ca51cfe15b19b9fa52d68fe71e949a7a597f7
forst09 <dforst09@gmail.com>
2024-04-16 19:41:35 +0300

    replace js to vue

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,13 +1,29 @@
-const btn = document.querySelector('button');
-const input = document.querySelector('input');
-const list = document.querySelector('ul');
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app');
 
-function addGoal() {
-    const value = input.value;
-    const listElement = document.createElement('li');
-    listElement.textContent = value;
-    list.append(listElement);
-    input.value = '';
-}
 
-btn.addEventListener('click', addGoal);
+// const btn = document.querySelector('button');
+// const input = document.querySelector('input');
+// const list = document.querySelector('ul');
+
+// function addGoal() {
+//     const value = input.value;
+//     const listElement = document.createElement('li');
+//     listElement.textContent = value;
+//     list.append(listElement);
+//     input.value = '';
+// }
+
+// btn.addEventListener('click', addGoal);
