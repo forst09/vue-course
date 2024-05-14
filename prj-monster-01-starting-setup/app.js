a29567d85d1b76ac2ac7ee997fc057698756572c
Дарья <dforst09@gmail.com>
2024-05-14 16:20:30 +0300

    start monster slayer project

---
+++ prj-monster-01-starting-setup/app.js
@@ -0,0 +1,55 @@
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const game = Vue.createApp({
+    data() {
+        return {
+            playerHealth: 100,
+            monsterHealth: 100,
+            currentRound: 0
+        }
+    },
+    computed: {
+        monsterBarStyles() {
+            return { width: this.monsterHealth + '%' }
+        },
+        playerBarStyles() {
+            return { width: this.playerHealth + '%' }
+        },
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 !== 0;
+        }
+    },
+    methods: {
+        attackMonster() {
+            const attackValue = getRandomValue(5, 12);
+            this.monsterHealth = this.monsterHealth - attackValue;
+            this.attackPlayer();
+            this.currentRound++;
+        },
+        attackPlayer() {
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth = this.playerHealth - attackValue;
+        },
+        specialAttackMonster() {
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth = this.monsterHealth - attackValue;
+            this.attackPlayer();
+            this.currentRound++;
+        },
+        healPlayer() {
+            this.currentRound++;
+            const healValue = getRandomValue(8, 20);
+            if (this.playerHealth + healValue > 100) {
+                this.playerHealth = 100;
+            }
+            else {
+                this.playerHealth = this.playerHealth + healValue;
+            }
+            this.attackPlayer();
+        }
+    }
+});
+
+game.mount('#game');
