07abc0477dc7283bd6197de86f392c1fbd58d519
Дарья <dforst09@gmail.com>
2024-05-16 16:21:50 +0300

    start learn components

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -5,55 +5,44 @@ const app = Vue.createApp({
       message: 'Vue is great!',
     };
   },
-  _methods: {
+  methods: {
     saveInput(event) {
       this.currentUserInput = event.target.value;
     },
     setText() {
-      // this.message = this.currentUserInput;
       this.message = this.$refs.userText.value;
-      // console.log(this.message = this.$refs.userText);
     },
   },
-  get methods() {
-    return this._methods;
+  beforeCreate() {
+    console.log('beforeCreate()');
   },
-  set methods(value) {
-    this._methods = value;
+  created() {
+    console.log('created()');
   },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted()');
+  }
 });
 
 app.mount('#app');
 
-const app2 = Vue.createApp({
-  template: `
-              <p>{{ hehe }}</p>
-            `,
-  data() {
-    return {
-      hehe: 'not hehe'
-    }
-  }
-});
+setTimeout(() => {
+  app.unmount();
+}, 3000);
 
-app2.mount('#app2')
 
-const data = {
-  message: 'hehe',
-  longMessage: 'hihihi'
-};
-
-const handler = {
-  set(target, key, value) {
-    if (key === 'message') {
-      target.longMessage = value + ' oh oh oh';
-    }
-    target.message = value;
-  }
-}
-
-const proxy = new Proxy(data, handler);
-
-proxy.message = 'not hehe';
-
-console.log(proxy.longMessage);
