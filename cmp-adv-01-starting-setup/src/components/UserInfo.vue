18f6740fc01ba76f1d262eab88bcb6986be49b7a
forst09 <dforst09@gmail.com>
2024-05-21 21:04:12 +0300

    start learn local components, scoped styles and slots

---
+++ cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <BaseCard>
+      <header>
+      <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </header>
+      <p>{{ infoText }}</p>    
+    </BaseCard>
+  </section>
+</template>
+
+<script>
+
+export default {
+  props: ['fullName', 'infoText', 'role'],
+};
+</script>
+
+<style scoped>
+section header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
