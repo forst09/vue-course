c30cb020f379f6d12f575200f95f935f9bfda147
Дарья <dforst09@gmail.com>
2024-05-24 14:16:27 +0300

    start learning resource project

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul>
+    <LearningResource v-for="resource in storedResource"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link" />
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+  data() {
+    return {
+      storedResource: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com'
+        }
+      ]
+    };
+  },
+  components: { LearningResource }
+}
+</script>
+
+<style scoped></style>
