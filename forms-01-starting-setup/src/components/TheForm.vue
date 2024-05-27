abd79c3d5d8a78117b41dc7e41aecdc8c29dd046
forst09 <dforst09@gmail.com>
2024-05-27 20:18:30 +0300

    complete forms module

--- forms-01-starting-setup/src/components/TheForm.vue
+++ forms-01-starting-setup/src/components/TheForm.vue
@@ -1,11 +1,14 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control"
+      :class="{ invalid: userNameValidity === 'invalid' }">
       <label for="user-name">Your Name</label>
       <input id="user-name"
         name="user-name"
         type="text"
-        v-model="userName" />
+        v-model.trim="userName"
+        @blur="validateInput" />
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid name!</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -80,6 +83,9 @@
       </div>
     </div>
     <div class="form-control">
+      <RatingControl v-model="rating"></RatingControl>
+    </div>
+    <div class="form-control">
       <input type="checkbox"
         id="confirm-terms"
         name="confirm-terms"
@@ -93,6 +99,8 @@
 </template>
 
 <script>
+import RatingControl from "./RatingControl.vue";
+
 export default {
   data() {
     return {
@@ -101,8 +109,13 @@ export default {
       referrer: 'wom',
       interest: [],
       how: null,
-      confirm: false
+      confirm: false,
+      userNameValidity: 'pending',
+      rating: null
     }
+  },
+  components: {
+    RatingControl
   },
   methods: {
     submitForm() {
@@ -122,6 +135,17 @@ export default {
       this.how = null;
       console.log('confirm');
       console.log(this.confirm);
+      console.log('rating');
+      console.log(this.rating);
+      this.rating = null;
+    },
+    validateInput() {
+      if (this.userName === '') {
+        this.userNameValidity = 'invalid';
+      }
+      else {
+        this.userNameValidity = 'valid';
+      }
     }
   }
 }
@@ -139,6 +163,14 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid label {
+  color: red;
 }
 
 label {
